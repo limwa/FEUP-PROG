046217e52f6bbfa4f73e0b9dd4b3011373482d4c
André Lima <me@limwa.pt>
2021-03-03 18:07:58 +0000

    feat: :sparkles: add solutions for exercises 2.13b and 2.13c

---
+++ exercise-sheet-01/2.13b/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+int readInteger(string prompt) {
+    while (true) {
+        int result;
+
+        cout << prompt << flush;
+        cin >> result;
+
+        if (!cin.fail())
+            return result;
+
+        cout << "Bad input! Please try again." << endl;
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
+
+int main(int, char**) {
+    float sum = 0, mean = 0, sd = 0;
+    int smallest = numeric_limits<int>::max();
+    int largest = numeric_limits<int>::min();
+    
+    vector<int> numbers;
+    while (int number = readInteger("Please specify a number: ")) {
+        numbers.push_back(number);
+
+        sum += number;
+        if (number < smallest)
+            smallest = number;
+
+        if (number > largest)
+            largest = number;
+    }
+
+    mean = sum / numbers.size();
+    
+    for (int number : numbers) {
+        sd += pow(number - mean, 2);
+    }
+
+    sd = sqrt(sd / (numbers.size() - 1));
+
+    cout << endl << numbers.size() << (numbers.size() == 1 ? " number was read." : " numbers were read.") << endl;
+    cout << endl << "Sum: " << sum << endl << "Mean: " << mean << endl << "Standard Deviation: " << sd << endl;
+    cout << endl << "Largest Value: " << largest << endl << "Smallest Value: " << smallest << endl;
+    return 0;
+}
---
+++ exercise-sheet-01/2.13c/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+int readInteger(string prompt) {
+    while (true) {
+        int result;
+
+        cout << prompt << flush;
+        cin >> result;
+
+        if (!cin.fail())
+            return result;
+
+        if (cin.eof())
+            break;
+
+        cout << "Bad input! Please try again." << endl;
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "EOF" << endl;
+    return 0;
+}
+
+int main(int, char**) {
+    float sum = 0, mean = 0, sd = 0;
+    int smallest = numeric_limits<int>::max();
+    int largest = numeric_limits<int>::min();
+    
+    vector<int> numbers;
+    while (true) {
+        int number = readInteger("Please specify a number: ");
+        if (cin.eof())
+            break;
+
+        numbers.push_back(number);
+
+        sum += number;
+        if (number < smallest)
+            smallest = number;
+
+        if (number > largest)
+            largest = number;
+    }
+
+    if (numbers.size() == 0)
+        return 1;
+
+    mean = sum / numbers.size();
+    
+    for (int number : numbers) {
+        sd += pow(number - mean, 2);
+    }
+
+    sd = sqrt(sd / (numbers.size() - 1));
+
+    cout << endl << numbers.size() << (numbers.size() == 1 ? " number was read." : " numbers were read.") << endl;
+    cout << endl << "Sum: " << sum << endl << "Mean: " << mean << endl << "Standard Deviation: " << sd << endl;
+    cout << endl << "Largest Value: " << largest << endl << "Smallest Value: " << smallest << endl;
+    return 0;
+}
